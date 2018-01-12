75d75d4f9a98f2daaabaf7fec462553d5549bc01
Liu Guo <1451606804@qq.com>
2018-01-12 19:39:47 +0800

    个人Activity的创建

--- app/src/main/java/com/xfwlkjyxgs/sober/poa/view/MainActivity.java
+++ app/src/main/java/com/xfwlkjyxgs/sober/poa/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.xfwlkjyxgs.sober.poa;
+package com.xfwlkjyxgs.sober.poa.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.xfwlkjyxgs.sober.poa.R;
 
 public class MainActivity extends AppCompatActivity {
 
