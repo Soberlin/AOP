acab6dfa5bb40ec7cc945136dc7e22ebcfb017d4
Liu Guo <1451606804@qq.com>
2018-01-12 19:42:06 +0800

    了解git怎么使用

--- app/src/main/java/com/xfwlkjyxgs/sober/poa/view/HomeActivity.java
+++ app/src/main/java/com/xfwlkjyxgs/sober/poa/view/HomeActivity.java
@@ -2,6 +2,8 @@ package com.xfwlkjyxgs.sober.poa.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.xfwlkjyxgs.sober.poa.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -12,6 +14,9 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
+    public  void  update(String msg){
+        Toast.makeText(this, "怎么创建一个本地仓库", Toast.LENGTH_SHORT).show();
+    }
 
 
 }
