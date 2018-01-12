cfc4b535ee826bc520c47234d447a0731655448f
Liu Guo <1451606804@qq.com>
2018-01-12 19:34:13 +0800

    HomeActivity的创建

---
+++ app/src/main/java/com/xfwlkjyxgs/sober/poa/view/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.xfwlkjyxgs.sober.poa.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.xfwlkjyxgs.sober.poa.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+
+
+}
