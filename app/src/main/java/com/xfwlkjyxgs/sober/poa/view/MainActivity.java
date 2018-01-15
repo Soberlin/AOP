b81de74d844704b5c912823e7ca004e6321bb0c4
Liu Guo <1451606804@qq.com>
2018-01-15 09:59:39 +0800

    测试当前系统时间  是否可用

--- app/src/main/java/com/xfwlkjyxgs/sober/poa/view/MainActivity.java
+++ app/src/main/java/com/xfwlkjyxgs/sober/poa/view/MainActivity.java
@@ -1,18 +1,34 @@
 package com.xfwlkjyxgs.sober.poa.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.xfwlkjyxgs.sober.poa.R;
+import com.xfwlkjyxgs.sober.poa.utils.MyTime;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_getCurrentTime)
+    Button btnGetCurrentTime;
+    @BindView(R.id.showTime)
+    TextView showTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
 
-
+    @OnClick(R.id.btn_getCurrentTime)
+    public void onClick() {
+       showTime.setText(MyTime.getSystemCurrentTime());
+    }
 }
