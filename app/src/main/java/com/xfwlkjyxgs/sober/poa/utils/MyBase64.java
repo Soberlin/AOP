a1973e781679302d0a9ad5649ec59cd5f7bc94ce
Liu Guo <1451606804@qq.com>
2018-01-17 10:05:46 +0800

    图片转换64

--- app/src/main/java/com/xfwlkjyxgs/sober/poa/utils/MyBase64.java
+++ app/src/main/java/com/xfwlkjyxgs/sober/poa/utils/MyBase64.java
@@ -6,10 +6,43 @@ package com.xfwlkjyxgs.sober.poa.utils;
  * Created data: Sober on 2018/1/16.
  * 你能我就能****
  */
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 /**
  * 图片转换为64位
  */
 public class MyBase64 {
 
+
+
+    public static String bitmapToBase64(Bitmap bitmap) {
+        String result = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            if (bitmap != null) {
+                baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG,30, baos);
+                byte[] bitmapBytes = baos.toByteArray();
+                result = Base64.encodeToString(bitmapBytes, Base64.NO_WRAP);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (baos != null) {
+                    baos.flush();
+                    baos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
 
 }
